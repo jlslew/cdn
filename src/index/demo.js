028fd2139e2e1775f5be724cf05d1bbb5472acbb
Jonathan Lee Slew <jonathan.leeslew@hotmail.com>
2020-09-04 15:23:56 +0400

    Updated demo

--- src/index/demo.js
+++ src/index/demo.js
@@ -1,4 +1,9 @@
+require(`construct-style-sheets-polyfill`);
 require(`systemjs/dist/system.js`);
+
+System.import(`${__webpack_public_path__}index.css`).then(callback => {
+    document.adoptedStyleSheets = document.adoptedStyleSheets.concat(callback.default);
+});
 
 System.import(`${__webpack_public_path__}index.js`).then($ => $.ready(() => ({
     view: () => m(``, `Hello`)
